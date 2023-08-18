9ea174b4fcad0513b81d5e9e4f46e23bfb6794f0
Renan Costa <renan.costa@enext.com.br>
2023-08-18 20:49:48 -0300

    feat: setup module path aliases

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,8 +1,10 @@
+import Logo from 'assets/images/svg/logo.svg';
 import './Home.style.scss'
 
 function Home() {
   return (
     <>
+      <img src={Logo} width={200} height={200} />
       <h1>Harry Potter</h1>
     </>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      components: path.resolve(__dirname, "./src/components"),
+      services: path.resolve(__dirname, "./src/services"),
+      contexts: path.resolve(__dirname, "./src/contexts"),
+      hooks: path.resolve(__dirname, "./src/hooks"),
+      utils: path.resolve(__dirname, "./src/utils"),
+      assets: path.resolve(__dirname, "./src/assets"),
+    }
+  }
 })
