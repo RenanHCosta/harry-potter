65605be8cbab5bd92a4b9d085c7126fbd1226299
Renan Costa <renan.costa@enext.com.br>
2023-08-21 02:20:06 -0300

    feat: add cool introduction text

--- src/components/presentation/Introduction/index.tsx
+++ src/components/presentation/Introduction/index.tsx
@@ -1,15 +1,24 @@
-import Logo from 'assets/images/svg/logo.svg';
-import Waves from 'components/ui/Waves';
+import Logo from "assets/images/svg/logo.svg";
+import Waves from "components/ui/Waves";
 
-import './Introduction.style.scss';
+import "./Introduction.style.scss";
 
 function Introduction() {
   return (
     <section className="introduction">
       <Waves />
-      <img className="logo" alt="Harry Potter Logo" src={Logo} width={200} height={200} />
+      <img
+        className="logo"
+        alt="Harry Potter Logo"
+        src={Logo}
+        width={200}
+        height={200}
+      />
+      <h1 className="introduction__title">
+        "A magia está em todos nós." - J.K. Rowling
+      </h1>
     </section>
-  )
+  );
 }
 
 export default Introduction;
